c5973a91cd7461eeef6eca52bb1c0dc5e11c673c
feelinflight <kolpacc123@mail.ru>
2020-04-16 16:17:07 +0500

    Теперь можно фильтровать тстлан с использованием *

--- tests_tree_widget.py
+++ tests_tree_widget.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List
 from enum import IntEnum
 import logging
 
--- tstlan_dialog.py
+++ tstlan_dialog.py
@@ -122,9 +122,12 @@ class TstlanDialog(QtWidgets.QDialog):
             logging.debug(utils.exception_handler(err))
 
     def filter_variables(self):
-        filter_text = self.ui.name_filter_edit.text()
+        regexp = QtCore.QRegExp(self.ui.name_filter_edit.text())
+        regexp.setPatternSyntax(QtCore.QRegExp.Wildcard)
+        regexp.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
         for i in range(self.ui.variables_table.rowCount()):
-            match = filter_text.lower() in self.ui.variables_table.item(i, self.Column.NAME).text()
+            # match = regexp.exactMatch(self.ui.variables_table.item(i, self.Column.NAME).text())
+            match = False if regexp.indexIn(self.ui.variables_table.item(i, self.Column.NAME).text()) == -1 else True
             if self.ui.show_marked_checkbox.isChecked():
                 marked_cb = self.ui.variables_table.cellWidget(i, self.Column.MARK).layout().itemAt(0).widget()
                 match = match & marked_cb.isChecked()
