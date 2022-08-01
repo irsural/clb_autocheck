f3611a747b9a3c1b23875ef27c2c7d4219cdc545
feelinglight <kolpacc123@mail.ru>
2022-08-01 09:26:54 +0500

    Сборка в exe переписана с использованием irspy

--- compile.py
+++ compile.py
@@ -1,23 +1,26 @@
-import os
+import irspy.pyinstaller_build as py_build
 
-
-def delete_convert_ui_strings(a_tmp_file):
-    with open("main.py") as main_py:
-        with open(a_tmp_file, "w+") as compile_main:
-            for line in main_py:
-                if not ("ui_to_py" in line):
-                    compile_main.write(line)
-
-
-def restore_convert_ui_strings(a_tmp_file):
-    os.remove(a_tmp_file)
+import app_info
 
 
 if __name__ == "__main__":
-    tmp_file = "main_to_compile.py"
+    app_info = py_build.AppInfo(a_app_name=app_info.SHORT_NAME,
+                                a_version=app_info.VERSION,
+                                a_company_name='ООО "РЭС"',
+                                a_file_description=app_info.FULL_NAME,
+                                a_internal_name=app_info.SHORT_NAME,
+                                a_copyright='ООО РЭС(c)',
+                                a_original_filename=app_info.FULL_NAME,
+                                a_product_name=app_info.FULL_NAME)
 
-    delete_convert_ui_strings(tmp_file)
-    os.system(f"pyinstaller -n clb_autocheck --onefile --noconsole --icon=main_icon.ico --add-data "
-              f"C:\\Windows\\System32\\vcruntime140d.dll;. --add-data C:\\Windows\\System32\\ucrtbased.dll;. "
-              f"{tmp_file}")
-    restore_convert_ui_strings(tmp_file)
+    libs = [
+        'C:\\Windows\\System32\\vcruntime140d.dll',
+        'C:\\Windows\\System32\\ucrtbased.dll',
+    ]
+
+    py_build.build_qt_app(a_main_filename="main.py",
+                          a_app_info=app_info,
+                          a_icon_filename="main_icon.ico",
+                          a_noconsole=True,
+                          a_one_file=True,
+                          a_libs=libs)
